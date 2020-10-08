49cb3c3d8ec644b23f03260abdf576b7e32b44d5
Alexander Frazis <alexanderfrazis@gmail.com>
2020-10-08 17:32:59 +0800

    Part1

--- BangBang2.c
+++ BangBang2.c
@@ -1,11 +1,17 @@
 #include "eyebot.h"
+#include <stdio.h>
 // #define HORIZONTAL_MAX 320
 // #define VERTICAL_MAX 240
 
 int v_des;  /* desired ticks/s */
+int enc_new1, v_act1;
+int count = 0;
+int prevCount = 0;
 void onoff_controller()
 { 
-    int enc_new1, v_act1, r_mot1;
+    count++;
+
+    int r_mot1;
     static int enc_old1;
     enc_new1 = ENCODERRead(1); 
     v_act1 = (enc_new1-enc_old1)*100; 
@@ -32,13 +38,31 @@ void onoff_controller()
 
 int main()
 { 
+    LCDPrintf("SRARTING");
+    FILE* ptr = fopen("/home/pi/usr/output.csv", "w");
     v_des = 10000;
-    TimerHandle t1;
-    t1 = OSAttachTimer(10, onoff_controller); 
+    TIMER t1;
+    t1 = OSAttachTimer(10, onoff_controller); //100ms
+    LCDMenu("","","","Quit");
     while (KEYRead() != KEY4) { /* check key */
+        // LCDPrintf("Encoder: %d \n", enc_new1);
+        // LCDPrintf("Encoder: %d \n", v_act1);
+        // LCDPrintf("Encoder: %d \n", count);
         
+        
+        // fprintf(ptr, "Encoder: %d \n", v_act1);
+        if (count != prevCount) {
+            prevCount = count;
+            fprintf(ptr, "%d,%d\n", enc_new1,v_act1);
+        }
+        if (count == 1000) {
+            fclose(ptr);
+            OSDetachTimer(t1);
+            break;
+            return 0;
+        }
         /* do other tasks, e.g. set speeds */ 
     } 
-    OSDetachTimer(t1);
+    
     return 0; 
 }
