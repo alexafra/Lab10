dd463b997274d41b0ff13ab503e2e723b3ccf516
Alexander Frazis <alexanderfrazis@gmail.com>
2020-10-08 16:03:52 +0800

    BangBang complete

--- BangBang.c
+++ BangBang.c
@@ -1,19 +1,42 @@
+#include "eyebot.h"
+
 int v_des;  /* desired ticks/s */
 void onoff_controller()
 { 
-    int enc_new, v_act, r_mot;
-    static int enc_old;
-    enc_new = ENCODERRead(1); 
-    v_act = (enc_new-enc_old)*100; 
-    if (v_act < v_des) {
-        r_mot = 100;
+    int enc_new1, v_act1, r_mot1;
+    static int enc_old1;
+    enc_new1 = ENCODERRead(1); 
+    v_act1 = (enc_new1-enc_old1)*100; 
+    if (v_act1 < v_des) {
+        r_mot1 = 100;
     } else {
-        r_mot = 0;  
+        r_mot1 = 0;  
     } 
-    MOTORDrive(1, r_mot);
-    enc_old = enc_new; 
+    MOTORDrive(1, r_mot1);
+    enc_old1 = enc_new1; 
+    
+    int enc_new2, v_act2, r_mot2;
+    static int enc_old2;
+    enc_new2 = ENCODERRead(2); 
+    v_act2 = (enc_new2-enc_old2)*100; 
+    if (v_act2 < v_des) {
+        r_mot2 = 100;
+    } else {
+        r_mot2 = 0;  
+    } 
+    MOTORDrive(2, r_mot2);
+    enc_old2 = enc_new2;
 }
 
-int main () {
-    
+int main()
+{ 
+    v_des = 10000;
+    TimerHandle t1;
+    t1 = OSAttachTimer(10, onoff_controller); 
+    while (KEYRead() != KEY4) { /* check key */
+        
+        * do other tasks, e.g. set speeds */ 
+    } 
+    OSDetachTimer(t1);
+    return 0; 
 }
