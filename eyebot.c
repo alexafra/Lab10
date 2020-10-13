3e992bc6b21689c49db4cca96b54634408ac0ce1
turnn <nick@elecnett.com.au>
2020-10-13 18:15:32 +0800

    Move definitions around

--- eyebot.c
+++ eyebot.c
@@ -40,13 +40,14 @@ int main()
 { 
     CAMInit(QQVGA);
     LCDImageSize(QQVGA);
-    LCDPrintf("STARTING");
-    FILE* ptr = fopen("/home/pi/usr/output.csv", "w");
+    BYTE myimage[QQVGA_SIZE];
+
+    /* FILE* ptr = fopen("/home/pi/usr/output.csv", "w");
     v_des = 10000;
     TIMER t1;
     t1 = OSAttachTimer(10, onoff_controller); //100ms
+     */
     LCDMenu("","","","Quit");
-
     
     //x = 320
     //y = 240
@@ -58,7 +59,7 @@ int main()
         // LCDPrintf("Encoder: %d \n", count);
         // fprintf(ptr, "Encoder: %d \n", v_act1);
 
-        if (count != prevCount) {
+        /* if (count != prevCount) {
             prevCount = count;
             fprintf(ptr, "%d,%d\n", enc_new1,v_act1);
         }
@@ -75,15 +76,15 @@ int main()
             OSDetachTimer(t1);
             break;
             return 0;
-        }
+        } */
         /* do other tasks, e.g. set speeds */ 
+
+        
+        CAMGet(myimage);
+        LCDImage(myimage);
     } 
     
     return 0; 
-}
-
-void CamToDisplay(){
-    LCDImage(CAMGet(colimage));
 }
 
 //Sure this can be found from a constant somewhere
@@ -104,15 +105,38 @@ void PrintHueToScreen(int[2] position){
     LCDSetPrintf(position[0], position[1], ("%d,%d",HueFromPosition(position)[0],HueFromPosition(position)[1]);
 }
 
-int[3] RGBFromPosition(int[] position){
+BYTE[3] RGBFromPosition(int[] position){
     CAMGet(colimage);
-    /* TO BE IMPLEMENTED */
+    int pixel = PositionToPixelIndex(position);
+    BYTE p_red = colimage[pixel*3];
+    BYTE p_green = colimage[pixel*3+1];
+    BYTE p_blue = colimage[pixel*3+2];
+    return [p_red,p_green,p_blue];
 }
 
-int[2] RGBToHSI(int[3] RGB){
-    /* TO BE IMPLEMENTED */
+BYTE[3] RGBToHSI(BYTE[3] RGB){
+    /* return hue value for RGB color */
+    #define NO_HUE 255
+    int hue, delta, max, min;
+    max = MAX(RGB[0], MAX(RGB[1],RGB[2]));
+    min = MIN(RGB[0], MIN(RGB[1],RGB[2]));
+    delta = max - min;
+    hue =0; /* init hue*/
+    if (2*delta <= max) hue = NO_HUE; /* gray, no color */
+    else {
+        if (RGB[0]==max) hue = 42 + 42*(RGB[1]-RGB[2])/delta; /* 1*42 */
+        else if (RGB[1]==max) hue = 126 +42*(RGB[2]-RGB[0])/delta; /* 3*42 */
+        else if (RGB[2]==max) hue = 210 +42*(RGB[0]-RGB[1])/delta; /* 5*42 */
+    }
+    sat = 1-(min/(RGB[0]+RGB[1]+RGB[2]));
+    intensity = (1/3)*(RGB[0]+RGB[1]+RGB[2]);
+    return [hue,sat,intensity];
 }
 
 int[2] HueFromPosition(int[] position){
     return RGBToHSI(RGBFromPosition(position));
+}
+
+int PositionToPixelIndex(int[] position){
+    return position[0]*width+position[1];
 }
